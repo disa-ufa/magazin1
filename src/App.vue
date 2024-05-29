7450554585d05841f3a85f1664f4ace5cc6f8d44
DenisRCDO <denisyxxx@mail.ru>
2024-05-29 16:58:37 +0500

    v29.05.24

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <script setup>
 import Headep from "./components/Headep.vue"
-
 import CardList from "./components/CardList.vue"
+import Drawer from "./components/Drawer.vue"
 </script>
 
 <template>
+  <!--<Drawer/>-->
   <div
     class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14"
   >
   <Headep/>
+
+  <div class="p-10">
+    <div class="flex justify-between items-center">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+      <div class="flex gap-4">
+      <select class="py-2 px-3 border rounded-md outline-none">
+        <option>По названию</option>
+        <option>Сначала дешевые</option>
+        <option>Сначала дорогие</option>
+      </select>
+      <div class="relative">
+        <img
+          class="absolute left-3 top-3"
+          src="/search.svg"
+        />
+        <input 
+        class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400" 
+        placeholder="Поиск"/>
+      </div>
+      </div>
+
+    </div>
+  </div>
+
   <CardList/>
 
   </div>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -8,18 +8,16 @@ const onClickAdd=()=>{
 <template>
 <div class="p-10">
     
-    <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+    
       <div class="grid grid-cols-4 gap-5">
         <Card 
         title="Кроссовки мужские Nike xxx" 
         img-url="/sneakers/sneakers-1.jpg" 
         :price="5000"
         :onClickAdd="onClickAdd"
+        :isAdded="true"
         />
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        
       </div>
     
   </div>
